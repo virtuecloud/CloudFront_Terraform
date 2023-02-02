c7d8cca3cfbe15c2c2ff8d677e27f375b21ff6fc
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2023-02-02 13:12:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
 module "cloudfront_with_s3" {
   source = "./module"
-  bucket_name = "jklmsadkankcj-dev"
+  bucket_name = var.bucket_name
+  bucketcreation = var.bucketcreation
+  cloudfront_min_ttl = var.cloudfront_min_ttl
+  cloudfront_default_ttl = var.cloudfront_default_ttl
+  cloudfront_max_ttl = var.cloudfront_max_ttl
+  price_class = var.price_class
+  upload_sample_file = var.upload_sample_file
+  tags = var.tags
+  cloudfront_geo_restriction_restriction_type = var.cloudfront_geo_restriction_restriction_type
   /* use_default_domain = "dev.entana.net" */
   /* acm_certificate_domain = "dev.entana.net" */
-  hosted_zone = "Z01684972SYKLONH5OMKA"
-  domain_name = "*.dev.entana.net"
-  cert_arn = "arn:aws:acm:us-east-1:342214740201:certificate/0d61fa72-58fd-4914-8b99-6c92978d1d3d"
+  /* hosted_zone = "Z01684972SYKLONH5OMKA" */
+  /* domain_name = "*.fonemedhms.com"
+  
+  cert_arn = "arn:aws:acm:ca-central-1:075687657552:certificate/546f6e8c-ffac-4e08-a749-65726311cc22" */
   # route_name ="cloudfront2.dev.entana.net"
   
 }
