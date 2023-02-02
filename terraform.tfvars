acfd695322af745252cc7194a84dd487f24e1c16
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2023-02-02 13:14:20 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,10 @@
+bucket_name = "ahgf"
+bucketcreation = true
+cloudfront_min_ttl = "0"
+cloudfront_default_ttl = "86400"
+cloudfront_max_ttl = "31536000"
+price_class = "PriceClass_100" // Only US,Canada,Europe
+upload_sample_file = false
+tags = {}
+cloudfront_geo_restriction_restriction_type = "none"
+
---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "bucket_name" {
+  
+}
+variable "bucketcreation" {
+  
+}
+variable "cloudfront_min_ttl" {
+  
+}
+variable "cloudfront_default_ttl" {
+  
+}
+variable "cloudfront_max_ttl" {
+  
+}
+variable "price_class" {
+  
+}
+variable "upload_sample_file" {
+  
+}
+variable "tags" {
+  
+}
+variable "cloudfront_geo_restriction_restriction_type" {
+  
+}
